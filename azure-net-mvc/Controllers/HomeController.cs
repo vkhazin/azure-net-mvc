057824c908dd358cd0f217348f026c0461811bcc
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2017-02-20 14:18:44 -0500

    Added call to app server

--- azure-net-mvc/Controllers/HomeController.cs
+++ azure-net-mvc/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,12 +10,29 @@ namespace azure_net_mvc.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        //TODO: move constants to config
+        private const string appUrl = "http://dev-allstreamexit-tscapp.azurewebsites.net/echo";
+        private const string headerKey = "x-app-key";
+        private const string xAppKey = "377dab30-da05-491b-9cee-e95ec6ec7cc1";
+
+        private Lazy<HttpClient> _httpClient = new Lazy<HttpClient>(() => new HttpClient());
+
+        public async Task<ActionResult> Index()
         {
             var webMsg = System.Environment.MachineName;
             ViewData["WebMsg"] = webMsg;
+            ViewData["AppMsg"] = (await this.GetAppEcho()).machineName;
             ViewData["Message"] = DateTime.UtcNow.ToString("O");
             return View();
+        }
+
+        private async Task<dynamic> GetAppEcho()
+        {
+            _httpClient.Value.DefaultRequestHeaders.Add(headerKey, xAppKey);
+            var response = await _httpClient.Value.GetAsync(appUrl);
+            response.EnsureSuccessStatusCode();
+            string json = await response.Content.ReadAsStringAsync();
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(json);
         }
     }
 }
